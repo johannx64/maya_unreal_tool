de2fc758f69cd5063c6386de2e647d12f8667c27
Johann S Cardenas <jscardenase@unal.edu.co>
2024-08-23 12:25:24 -0500

    unreal export batch fixed

--- unreal/exportAssets.py
+++ unreal/exportAssets.py
@@ -1,83 +1,69 @@
 import unreal
 import os
-assets_path = ".\\assets"
 
-# Specify the folder path where .fbx files are located
-folder_path = r'C:\\Users\\stefa\\Pictures\\unrealscript\\core\\maya\\'
+assets_path = r"C:\Users\stefa\Desktop\scatter_assets\Unreal_High"
 
-def exportSelectedMeshes():
+# Specify the root folder where the search for Static Meshes should start
+folder_path = r'/Game/Megascans/3D_Assets'  # Change this to your desired root folder in Unreal
+
+def export_all_meshes_in_folder(root_folder):
     """
-    Export selected static meshes to FBX files.
+    Recursively find and export all Static Meshes in the given folder and its subfolders.
     """
-    # Get selected assets from content browser
-    selectedAssets = unreal.EditorUtilityLibrary.get_selected_assets()
-    
-    # Check if any assets are selected
-    if not selectedAssets:
-        unreal.log("No assets selected for export!")
+    asset_registry = unreal.AssetRegistryHelpers.get_asset_registry()
+
+    # Search for all assets in the folder and subfolders
+    asset_filter = unreal.ARFilter(
+        package_paths=[root_folder],
+        recursive_paths=True,
+        class_names=["StaticMesh"]
+    )
+
+    found_assets = asset_registry.get_assets(asset_filter)
+
+    if not found_assets:
+        unreal.log("No assets found for export in the specified folder.")
         return
-    
-    unreal.log("Starting export of selected static meshes...")
-    
-    for selectedAsset in selectedAssets:
-        if isinstance(selectedAsset, unreal.StaticMesh):
-            assetName = selectedAsset.get_name()
-            unreal.log("Exporting mesh: {}".format(assetName))
-            
+
+    unreal.log("Starting export of Static Meshes from folder: {}".format(root_folder))
+
+    for asset_data in found_assets:
+        asset = asset_data.get_asset()
+
+        if isinstance(asset, unreal.StaticMesh):
+            asset_name = asset.get_name()
+            unreal.log("Exporting mesh: {}".format(asset_name))
+
             # Create asset export task
-            exportTask = unreal.AssetExportTask()
-            exportTask.automated = True
-            exportTask.object = selectedAsset
-            exportTask.prompt = False
-            
-            # Set the filename and export options
-            exportTask.filename = assets_path + "\\" + assetName + '.fbx'
-            exportTask.options = unreal.FbxExportOption()
-            fbxExporter = unreal.StaticMeshExporterFBX()
-            exportTask.exporter = fbxExporter
-            
-            # Run export task
-            result = fbxExporter.run_asset_export_task(exportTask)
+            export_task = unreal.AssetExportTask()
+            export_task.automated = True
+            export_task.object = asset
+            export_task.prompt = False
+
+            # Set the filename
+            export_task.filename = os.path.join(assets_path, asset_name + '.fbx')
+
+            # Create Fbx export options and set the required options
+            export_options = unreal.FbxExportOption()
+            export_options.export_source_mesh = True  # Export only the highest LOD (source mesh)
+            export_options.level_of_detail = False    # Disable exporting other LODs
+            export_task.options = export_options
+
+            # Create the FBX exporter
+            fbx_exporter = unreal.StaticMeshExporterFBX()
+            export_task.exporter = fbx_exporter
+
+            # Run the export task
+            result = fbx_exporter.run_asset_export_task(export_task)
             if result:
-                unreal.log("Successfully exported Static Mesh: {}".format(assetName))
+                unreal.log("Successfully exported Static Mesh: {}".format(asset_name))
             else:
-                unreal.log_error("Failed to export Static Mesh: {}".format(assetName))
+                unreal.log_error("Failed to export Static Mesh: {}".format(asset_name))
+
         else:
-            unreal.log_warning("Asset type not supported for export: {}".format(selectedAsset.get_name()))
-    
+            unreal.log_warning("Asset type not supported for export: {}".format(asset_data.asset_name))
+
     unreal.log("Mesh export process completed.")
 
- # Function to import FBX file into Unreal
-
-def import_fbx(file_path, destination_path):
-    fbx_import_ui = unreal.FbxImportUI()
-    fbx_import_ui.automated_import_should_detect_type = True
-    fbx_import_ui.import_materials = True
-    fbx_import_ui.import_textures = True
-    fbx_import_ui.import_as_skeletal = False
-    fbx_import_ui.import_animations = False
-    
-    # Define the import task
-    task = unreal.AssetImportTask()
-    task.filename = file_path
-    task.destination_path = destination_path
-    task.destination_name = ""
-    task.automated = True
-    task.save = True
-    task.options = fbx_import_ui
-    
-    # Import the FBX
-    unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])
-    
-def importAssets():
-    # Search for all .fbx files in the folder
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(".fbx"):
-                file_path = os.path.join(root, file)
-                destination_path = "/Game/imports2/"  # Specify your Unreal destination path here
-                import_fbx(file_path, destination_path)
-                print(f"Imported: {file_path}")
-
-    print("All FBX files have been imported.")
-
+# Call the function to export all meshes in the folder (including subfolders)
+export_all_meshes_in_folder(folder_path)
